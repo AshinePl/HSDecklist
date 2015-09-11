1a182685a4ac885d757e12c043e556054f241897
AshinePl <aanndyy@gmail.com>
2015-09-11 23:13:15 +0200

    FindingCard() added

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -22,45 +22,23 @@ using namespace std;
 
 int main()
 {
-	PGconn* conn = PQconnectdb("dbname=postgres user=postgres password=1234");
 
-	if (PQstatus(conn) == CONNECTION_BAD)	{
-		cout << "ERROR: " << PQerrorMessage(conn) << endl;
-	}
-	else	{
-		cout << "Connection OK" << endl;
-	}
+	Deck d;
+	bool _running;
 
-	/* for testing purposes
-	Deck deck;
-	deck.Add(new Card("Andrzej", 3));
-	deck.Add(new Minion("Tomek", 6, 3, 8));
-	deck.Add(new Card("Maciek", 3),1);
-	deck.Add(new Card("Aleksandra", 3));
-	deck.Add(new Minion("Iza", 8, 8, 8));
-	deck.Add(new Card("Pawel", 3),1);
-	deck.Add(new Minion("Nabuchodonozor 2000", 10, 1, 10), 1);
+	do {
+		// if there are no connection then exit
+		if (PQstatus(d.GetConnection()) == CONNECTION_BAD) { 
+			return 0; 
+		}
 
-	deck.UpdateLength();
-	bool _running = true;
-	do
-	{
-		deck.Show();
-		_running = deck.Menu();
+		d.Show();
+		_running = d.Menu();
 
-
-		system("cls");
 	} while (_running);
-	//*/
 
-	/*
-	for (int i = 0; i < 255; i++)
-	{
-	printf("%d: %c\n", i, i);
-	}
-	//*/
+	
 
-	PQfinish(conn);
 	system("pause");
 
 	return 0;
--- ConsoleApplication8/Deck.h
+++ ConsoleApplication8/Deck.h
@@ -3,8 +3,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <iomanip>
 #include <list>
+
+#include <libpq-fe.h>
+#pragma comment(lib, "libpq.lib")
 
 using namespace std;
 
@@ -124,6 +128,7 @@ public:
 class Deck {
 	// single deck 
 	list<Decklist*> _l;
+	PGconn* _conn;
 	int _quantity;
 	int _nameLength;
 	char _border[11]; // for Show() function
@@ -139,9 +144,20 @@ public:
 	int GetQuantity() const {
 		return _quantity;
 	}
+	PGconn* GetConnection() const {
+		return _conn;
+	}
 
 	// --- Construtors / Destructor ---------------
 	Deck() : _quantity(0), _nameLength(0)  {
+		// connecting to SQL
+		_conn = PQconnectdb("dbname=postgres user=postgres password=1234");
+		if (PQstatus(_conn) == CONNECTION_BAD)	{
+			cout << "ERROR: " << PQerrorMessage(_conn) << endl;
+			PQfinish(_conn);
+		}
+		cout << "Connection OK" << endl;
+
 		_border[0] = (char)185; // ╣
 		_border[1] = (char)186; // ║
 		_border[2] = (char)187; // ╗
@@ -155,6 +171,7 @@ public:
 		_border[10] = (char)206; // ╬
 	}
 	~Deck() {
+		PQfinish(_conn);
 		list<Decklist*>::iterator it;
 		for (it = _l.begin(); it != _l.end(); ++it){
 			delete (*it);
@@ -169,6 +186,100 @@ public:
 			_nameLength = newLen;
 		_l.push_back(new Decklist(c, q));
 	}
+
+	Card* FindCard(const char* name) {
+		stringstream query;
+		query << "SELECT t_name AS Name, t_cost AS Cost, t_type AS Type, t_attack AS Attack, t_health AS Health FROM tab WHERE t_name ~ '";
+		query << name;
+		query << "';";
+
+		// Query
+		PGresult* result = PQexec(_conn, query.str().c_str());
+
+		ExecStatusType status = PQresultStatus(result);
+		if (status == PGRES_TUPLES_OK)
+		{
+			int rows = PQntuples(result);
+			int cols = PQnfields(result);
+			// columns names
+			cout << "ID ";
+			for (int j = 0; j < cols; j++)
+			{
+				cout << PQfname(result, j) << ' ';
+			}
+			cout << endl;
+			// searching for longest name
+			int le = 0;
+			for (int i = 0; i < rows; i++)
+			{
+				if (strlen(PQgetvalue(result, i, 0)) > le)
+					le = strlen(PQgetvalue(result, i, 0));
+			}
+
+			cout << endl;
+			for (int i = 0; i < le + 28; i++)
+				cout << '-';
+			cout << endl;
+
+			// printing list
+			int i = 0;
+			for (; i < rows; i++)
+			{
+				cout << setw(2) << i + 1 << ' ';
+
+				char * ch_result = PQgetvalue(result, i, 0);
+				cout << setw(le) << ch_result << ' '; //Name
+
+				ch_result = PQgetvalue(result, i, 1);
+				cout << setw(4) << ch_result << ' '; // Cost
+
+				ch_result = PQgetvalue(result, i, 2);
+				cout << setw(4) << ch_result << ' '; // Type
+
+				if (!strcmp(ch_result, "M")) {
+					ch_result = PQgetvalue(result, i, 3);
+					cout << setw(6) << ch_result << ' '; // Attack
+
+					ch_result = PQgetvalue(result, i, 4);
+					cout << setw(6) << ch_result << ' '; // Health
+				}
+
+				cout << endl;
+			}
+			cout << endl << "Add Card with ID: ";
+
+			string c;
+			cin >> c;
+			int num = atoi(c.c_str());
+
+			if (num == 0 || num > i + 1) return NULL;
+
+			else if (!strcmp(PQgetvalue(result, num - 1, 2), "M")) { // if Minion 
+				return new Minion(
+					PQgetvalue(result, num - 1, 0), // Name
+					atoi(PQgetvalue(result, num - 1, 1)), // Cost
+					atoi(PQgetvalue(result, num - 1, 3)), // Attack
+					atoi(PQgetvalue(result, num - 1, 4)) // Health
+					);
+			}
+			else { // Spell Card
+				return new Card(
+					PQgetvalue(result, num - 1, 0), // Name
+					atoi(PQgetvalue(result, num - 1, 1)) // Cost
+					);
+			}
+
+		}
+		else {
+			PQclear(result);
+			return NULL;
+		}
+
+		//Clearing
+		PQclear(result);
+
+	}
+
 	void WriteHeader() {
 		// ╔════╦═
 		std::cout << _border[5] << _border[9] << _border[9] << _border[9] << _border[9]
@@ -256,12 +367,12 @@ public:
 		unsigned int newlen = 0;
 		list<Decklist*>::iterator it = _l.begin();
 		for (; it != _l.end(); ++it) {
-			if ((**it).GetQuantity() > 0 && newlen < strlen((**it).GetCard()->GetName().c_str())) {	
+			if ((**it).GetQuantity() > 0 && newlen < strlen((**it).GetCard()->GetName().c_str())) {
 				// if Qunatity > 0 and Length of card more then newlen
 				newlen = strlen((**it).GetCard()->GetName().c_str());
 			}
 		}
-		if (newlen < _nameLength) {	_nameLength = newlen; } // updating _nameLength
+		if (newlen < _nameLength) { _nameLength = newlen; } // updating _nameLength
 	}
 
 	int Draw(int number_of_cards) { // Decrease number of cards in Deck with given id
@@ -272,6 +383,7 @@ public:
 			if (!(**it).Decrease()) UpdateLength(); // if Decrease() returned 0 then update length
 		}
 	}
+
 	bool Menu()	{
 		char c;
 		std::cout << "A - Add Cards to Deck" << endl;
@@ -286,9 +398,32 @@ public:
 
 		cin >> c; // choice of user
 		if (c == '0') return false;
-		
-		switch (c){
-			// TODO:
+
+	
+
+		switch (toupper(c)){
+			//======================================
+		case 'A':
+		{
+			string name;
+			cout << "Podaj nazwe karty: ";
+			cin >> name;
+			Card* newCard = FindCard(name.c_str());
+			do {
+				cout << "Podaj ilość: ";
+				cin >> name;
+				if (atoi(name.c_str())) { break; }
+				else cout << "Błędna ilość" << endl;
+			} while (true);
+			Add(newCard, atoi(name.c_str()));
+			break;
+		}
+			//===========================================
+		case 'D':
+		{
+
+		}
+
 		}
 		return true;
 	}
@@ -297,6 +432,7 @@ public:
 
 // Prototypes
 //--- Printing Card with given Name Width --------------
+
 void PrintCard(Card* c, int nameW){
 	Minion* m = dynamic_cast<Minion*>(c);
 	if (m == 0) {
